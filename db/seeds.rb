6dabd1f92b00b49b4d562affa4315456f14d3085
Aram Sulukdjian <asulukdjian@pm.me>
2020-11-11 12:46:58 +0100

    restaurant index and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
 5.times do
   restaurant = Restaurant.new(
     name: Faker::Company.name,
     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    phone_number: Faker::PhoneNumber.cell_phone
   )
   restaurant.save!
 end
